2173cbcb9984dcaa07356dd61471e030136e739a
Lazar <246503773+LazarMilosevic@users.noreply.github.com>
2026-07-19 15:48:59 +0200

    adjusted height spawning range of pipes

--- Assets/Scripts/PipeSpawner.cs
+++ Assets/Scripts/PipeSpawner.cs
@@ -4,11 +4,10 @@ using UnityEngine;
 public class PipeSpawner : MonoBehaviour
 {
     [SerializeField] private float _maxTime = 3f;
-    [SerializeField] private float _heigthRange = 0.67f;
+    [SerializeField] private float _heigthRange = 0.65f;
     [SerializeField] private GameObject _pipeOriginal;
 
     private float _timer;
-
 
     void Start()
     {
